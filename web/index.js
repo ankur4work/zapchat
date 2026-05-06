c8583b04d222ff8e0b6ed4a5f4d0c18921a68b32
ankursing4u <a4ankur.mail@gmail.com>
2026-05-06 16:42:11 +0530

    chore: set IS_TEST=true for dev store subscription testing

--- web/index.js
+++ web/index.js
@@ -30,7 +30,7 @@ const APP_NAMESPACE = "custom";
 const SHOP_METAFIELD_KEY = "zapchat-whatsapp-button";
 const APP_INSTALL_METAFIELD_KEY = "zapchat-whatsapp-button-premium";
 
-const IS_TEST = false;
+const IS_TEST = true;
 
 const APP_NAME = "zapchat-whatsapp-button";
 
